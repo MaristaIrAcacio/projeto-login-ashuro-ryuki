f1c33f080d8d19d2031a2f44c1a5851d22bd9a5c
Leonardo Fernando <lffxds@gmai.com>
2023-07-26 11:16:05 -0300

    recuperação dos dados perdidos

---
+++ controller.js
@@ -0,0 +1,84 @@
+function acessar() {
+
+    let loginEmail = document.getElementById("loginEmail").value;
+
+    let loginSenha = document.getElementById("loginSenha").value;
+
+ 
+
+    if(!loginEmail || !loginSenha) {
+
+        alert("favor preencha os campos para acessar o site");
+
+    } else {
+
+       window.location.href = "aula2.html";
+
+    }
+
+}
+
+
+
+
+// cria lista de usuario dentro de um array
+
+
+
+
+var dadoslista = [];
+
+
+
+
+function salvarUser(){
+
+    let nomeUser = document.getElementById("nomeUser").value;
+
+
+
+
+    if(nomeUser){
+
+        dadoslista.push(nomeUser);
+
+        criaLista();
+
+        document.getElementById("nomeUser").value = '';
+
+        }else{
+
+            alert("usuario, por favor preencher o campo nome");
+
+        }
+
+    }
+
+
+
+
+
+
+// FUNÇÃO PARA CRIAÇÃO DE LISTA NA TABELA
+
+function criaLista(){
+
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>nomeUsuario</th><th>açôes</th></tr>";
+
+
+
+
+    // LAÇO DE REPETIÇÃO PARA COLOCAR NOMES NA TABELA
+
+    for(let i=0; i <= (dadoslista.length-1); i++){
+
+        tabela += "<tr><td>" + dadoslista[i] + "</td><td><button class='btn btn-success'onclick''>Editar<button><button class='btn btn-danger'onclick''>Excluir</button></td></tr>";
+
+              document.getElementById("tabela"). innerHTML = tabela;
+
+    }
+
+
+
+
+}
